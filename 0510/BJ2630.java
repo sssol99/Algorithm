6748a45da772865417cb0c90c5028e084f3cbde9
sssol99 <qwertty16@naver.com>
2022-05-10 21:34:06 +0900

    수정

--- 0510/BJ2630.java
+++ 0510/BJ2630.java
@@ -5,8 +5,7 @@ import java.util.StringTokenizer;
 
 public class BJ2630 {
 	static int arr[][];
-	
-	//static boolean ck[][];
+
 	static int white; //하양갯수저장
 	static int blue; //파랑갯수저장
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -15,9 +14,8 @@ public class BJ2630 {
 		int N = Integer.parseInt(br.readLine());
 		white = 0; //하양갯수저장
 		blue = 0; //파랑갯수저장
-		//ck = new boolean[N][N];
-		arr = new int[N][N];
 
+		arr = new int[N][N];
 		for(int i = 0 ; i < N ; i++) {
 			StringTokenizer stk = new StringTokenizer(br.readLine());
 			for(int j = 0 ; j < N ; j++) {
@@ -26,7 +24,6 @@ public class BJ2630 {
 		}
 
 		//알고리즘 구현입니다
-
 		DFS(0,0,N);
 		System.out.println(white);
 		System.out.println(blue);
@@ -34,26 +31,23 @@ public class BJ2630 {
 
 	private static void DFS(int x, int y, int n) {
 		int tmp = 0;
-		for(int i = x ; i < x+n ; i++) {
-			for(int j = y ; j < y+n ; j++){
-				tmp+=arr[i][j];
+		for(int i = x ; i < x+n ; i++) { // 지금x 위치 + n만큼
+			for(int j = y ; j < y+n ; j++){ //지금 y위치 + n만큼
+				tmp+=arr[i][j]; // 
 			}
 		}
 
-		if(tmp == n*n) {
+		if(tmp == n*n) { // 다 1이면 blue
 			blue++;
 			return;
-		}else if(tmp==0) {
+		}else if(tmp==0) { //다 0이면 white
 			white++;
 			return;
-		}else {
+		}else {//아니면 또 쪼개야함
 			DFS(x,y,n/2); //2사분면
 			DFS(x,y+(n/2),n/2);//3사분면
 			DFS(x+(n/2),y,n/2);//1사분면
 			DFS(x+(n/2),y+(n/2),n/2);//4사분면
 		}
-
-
 	}
-
 }
